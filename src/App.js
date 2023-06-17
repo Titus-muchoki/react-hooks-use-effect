5f36b9d90deecbdf405a9b06f608c58f93af51bb
kajela <titoyut56@gmail.com>
2023-06-17 12:26:03 -0400

    add the initial files

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState("");
 
+  useEffect(() => {
+    document.title = text;
+  }, [text]);
+
 
   useEffect(() => {
-    console.log("useEffect called");
-    // [count]
+    setTimeout(() => setCount(0), 5000);
+    
   }, []);
 
   console.log("Component rendering");
